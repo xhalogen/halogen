64f244afcf846583c7e9d56f377ca34c298b462b
zhaoxiuya <zhaoxiuya13@gmail.com>
2026-02-09 23:30:20 +0900

    feat: arthimetic

---
+++ include/absalg/arithmetic.hpp
@@ -0,0 +1,64 @@
+#include "absalg.hpp"
+#include <functional>
+#include <type_traits>
+
+namespace halogen {
+    template<typename T>
+    requires std::is_arithmetic_v<T>
+    struct identity_traits<std::plus<T>, T> {
+        static constexpr std::optional<T> value() { return T(0); };
+    };
+
+    template<typename T>
+    requires std::is_arithmetic_v<T>
+    struct identity_traits<std::multiplies<T>, T> {
+        static constexpr std::optional<T> value() { return T(1); };
+    };
+
+    template<typename T>
+    requires std::is_arithmetic_v<T>
+    struct inverse_traits<std::plus<T>, T> {
+        static constexpr std::optional<T> of(T a) {
+            return -a;
+        };
+    };
+
+    template<typename T>
+    requires std::is_floating_point_v<T>
+    struct inverse_traits<std::multiplies<T>, T> {
+        static constexpr std::optional<T> of(T a) {
+            if (a == T(0)) return std::nullopt;
+            return T(1) / a;
+        };
+    };
+
+    template<typename T>
+    requires std::is_floating_point_v<T>
+    struct r_div_traits<std::multiplies<T>, T> {
+        static constexpr std::optional<T> of(T a, T b) {
+            if (b == T(0)) return std::nullopt;
+            return a / a;
+        };
+    };
+
+    template<typename T>
+    requires std::is_floating_point_v<T>
+    struct l_div_traits<std::multiplies<T>, T> {
+        static constexpr std::optional<T> of(T a, T b) {
+            if (b == T(0)) return std::nullopt;
+            return b / a;
+        };
+    };
+
+    template<typename F, typename T>
+    requires std::is_arithmetic_v<T>
+    struct associative<F, T> : std::true_type {};
+
+    template<typename F, typename T>
+    requires std::is_arithmetic_v<T>
+    struct commutative<F, T> : std::true_type {};
+
+    template<typename T>
+    requires std::is_arithmetic_v<T>
+    struct distributive<std::plus<T>, std::multiplies<T>, T> : std::true_type {};
+};
