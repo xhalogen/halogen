c74832021979663423e19560a4d80996f283cfc1
zhaoxiuya <zhaoxiuya13@gmail.com>
2026-02-21 17:39:22 +0900

    chore: initialized cargo

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Nix flake for Project Halogen";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            git
+            direnv
+            rustc
+            cargo
+            rustfmt
+            clippy
+            rust-analyzer
+          ];
+        };
+      }
+    );
+}
